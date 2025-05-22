94c99240079874a249a14426636e4c276b1f17f5
isaac <isaackodak@gmail.com>
2025-05-22 16:20:12 -0500

    cambio ocnfig.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import { config } from "dotenv";
 config();
 
-export const DB_HOST = process.env.BD_HOST || "metro.proxy.rlwy.net";
+export const DB_HOST = process.env.BD_HOST || "mysql.railway.internal";
 export const DB_DATABASE = process.env.BD_DATABASE || "railway";
 export const DB_USER = process.env.BD_USER || "root";
-export const DB_PASSWORD = process.env.BD_PASSWORD || "mysql://root:iaVXlcIoysGlOSiZPwHTSGLixHHeLpUf@mysql.railway.internal:3306/railway";
-export const DB_PORT = process.env.BD_PORT || 43819;
+export const DB_PASSWORD = process.env.BD_PASSWORD || "iaVXlcIoysGlOSiZPwHTSGLixHHeLpUf";
+export const DB_PORT = process.env.BD_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
