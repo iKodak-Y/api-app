52cdb1100dbc3d0e58aae671b2dcb23df249d2ad
isaac <isaackodak@gmail.com>
2025-05-22 16:47:19 -0500

    agg verificarToken clientes route

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -11,11 +11,11 @@ import { verificarToken } from "../auth.js";
 
 const router = Router();
 
-router.get("/cliente", getClientes);
-router.get("/cliente/:id", getCliente);
-router.post("/cliente", postCliente);
-router.put("/cliente/:id", putCliente);
-router.patch("/cliente/:id", patchCliente);
-router.delete("/cliente/:id", deleteCliente);
+router.get("/cliente", verificarToken, getClientes);
+router.get("/cliente/:id", verificarToken, getCliente);
+router.post("/cliente", verificarToken, postCliente);
+router.put("/cliente/:id", verificarToken, putCliente);
+router.patch("/cliente/:id", verificarToken, patchCliente);
+router.delete("/cliente/:id", verificarToken, deleteCliente);
 
 export default router;
