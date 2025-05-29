4fe15e0ba3739ec55a7a848e09c3ebc958441ef2
isaac <isaackodak@gmail.com>
2025-05-29 13:35:38 -0500

    add get cliente por ci corr

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -13,7 +13,7 @@ const router = Router();
 
 router.get("/cliente", getClientes);
 router.get("/cliente/:id", getCliente);
-router.get("/cliente/:cli_identificacion", getClientexCI);
+router.get("/cliente/ci/:cli_identificacion", getClientexCI);
 router.post("/cliente", postCliente);
 router.put("/cliente/:id", putCliente);
 router.patch("/cliente/:id", patchCliente);
