96b5ea05ff1a2e17771e7d3fa987f019eedd651b
isaac <isaackodak@gmail.com>
2025-05-29 14:52:12 -0500

    cambio ret

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -1,104 +1,81 @@
-import { conmysql } from "../bd.js";
+import {conmysql} from '../bd.js'
 
-export const getPedidos = async (req, res) => {
-  try {
-    const [result] = await conmysql.query("select * from pedidos");
-    res.json({ cant: result.length, data: result });
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const getPedidos = async (req,res)=>{
+    try {
+        const [result] = await conmysql.query('select * from pedidos')
+        res.json({cant: result.length, data:result})
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const getPedidosID = async (req, res) => {
-  try {
-    const [result] = await conmysql.query(
-      "select * from pedidos where ped_id=?",
-      [req.params.id]
-    );
-    if (result.length <= 0)
-      return res.status(400).json({
-        ped_id: 0,
-        message: "Pedido no encontrado",
-      });
-    res.json(result[0]);
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const getPedidosID = async (req,res)=>{
+    try {
+        const [result] = await conmysql.query('select * from pedidos where ped_id=?', [req.params.id])
+        if(result.length<=0) return res.status(400).json({
+            ped_id : 0,
+            message : "Pedido no encontrado"
+        })
+        res.json(result[0])
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const postPedidos = async (req, res) => {
-  try {
-    const { cli_id, ped_fecha, usr_id, ped_estado } = req.body;
-    const [result] = await conmysql.query(
-      "INSERT INTO pedidos(cli_id, ped_fecha, usr_id, ped_estado) VALUES (?,?,?,?)",
-      [cli_id, ped_fecha, usr_id, ped_estado]
-    );
-    res.json({
-      ped_id: result.insertId,
-      message: "Pedido creado exitosamente",
-    });
-  } catch (error) {
-    console.error("Error al crear pedido:", error);
-    return res
-      .status(500)
-      .json({ message: "Error en el servidor al crear el pedido" });
-  }
-};
+export const postPedidos = async (req,res)=>{
+    try {
+        const {cli_id, ped_fecha,usr_id, ped_estado} = req.body
+        const [result] = await conmysql.query('INSERT INTO pedidos(cli_id, ped_fecha, usr_id, ped_estado) VALUES (?,?,?,?)', [cli_id, ped_fecha, usr_id, ped_estado])
+        res.send({
+            id: result.insertId
+        })
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const putPedido = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { cli_id, ped_fecha, usr_id, ped_estado } = req.body;
-    const [result] = await conmysql.query(
-      "UPDATE pedidos SET cli_id=?,ped_fecha=?,usr_id=?,ped_estado=? WHERE ped_id=?",
-      [cli_id, ped_fecha, usr_id, ped_estado, id]
-    );
-    if (result.length <= 0)
-      return res.status(404).json({
-        message: "Pedido no encontrado",
-      });
-    const [row] = await conmysql.query("select * from pedidos where ped_id=?", [
-      id,
-    ]);
-    res.json(row[0]);
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const putPedido = async (req,res)=>{
+    try {
+        const {id} = req.params
+        const {cli_id, ped_fecha, usr_id, ped_estado} = req.body
+        const [result] = await conmysql.query(
+            'UPDATE pedidos SET cli_id=?,ped_fecha=?,usr_id=?,ped_estado=? WHERE ped_id=?', 
+            [cli_id, ped_fecha, usr_id, ped_estado, id])
+        if(result.length<=0) return res.status(404).json({
+            message : "Pedido no encontrado"
+        })
+         const [row] = await conmysql.query('select * from pedidos where ped_id=?', [id])
+         res.json(row[0])
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const patchPedido = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { cli_id, ped_fecha, usr_id, ped_estado } = req.body;
-    const [result] = await conmysql.query(
-      "UPDATE pedidos SET cli_id=IFNULL(?,cli_id),ped_fecha=IFNULL(?,ped_fecha),usr_id=IFNULL(?,usr_id),ped_estado=IFNULL(?,ped_estado) WHERE ped_id=?",
-      [cli_id, ped_fecha, usr_id, ped_estado, id]
-    );
-    if (result.length <= 0)
-      return res.status(404).json({
-        message: "Pedido no encontrado",
-      });
-    const [row] = await conmysql.query("select * from pedidos where ped_id=?", [
-      id,
-    ]);
-    res.json(row[0]);
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const patchPedido = async (req,res)=>{
+    try {
+        const {id} = req.params
+        const {cli_id, ped_fecha, usr_id, ped_estado} = req.body
+        const [result] = await conmysql.query(
+            'UPDATE pedidos SET cli_id=IFNULL(?,cli_id),ped_fecha=IFNULL(?,ped_fecha),usr_id=IFNULL(?,usr_id),ped_estado=IFNULL(?,ped_estado) WHERE ped_id=?', 
+            [cli_id, ped_fecha, usr_id, ped_estado, id])
+        if(result.length<=0) return res.status(404).json({
+            message : "Pedido no encontrado"
+        })
+         const [row] = await conmysql.query('select * from pedidos where ped_id=?', [id])
+         res.json(row[0])
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const deletePedido = async (req, res) => {
-  try {
-    const [result] = await conmysql.query(
-      "delete from pedidos where ped_id=?",
-      [req.params.id]
-    );
-    if (result.length <= 0)
-      return res.status(400).json({
-        message: "Pedido no encontrado",
-      });
-    res.sendStatus(204);
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const deletePedido = async (req,res)=>{
+    try {
+        const [result] = await conmysql.query('delete from pedidos where ped_id=?', [req.params.id])
+        if(result.length<=0) return res.status(400).json({
+            message : "Pedido no encontrado"
+        })
+        res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
--- src/controllers/pedidosdetalle.controllers.js
+++ src/controllers/pedidosdetalle.controllers.js
@@ -1,106 +1,83 @@
-import { conmysql } from "../bd.js";
+import {conmysql} from '../bd.js'
 
-export const getPedidosDetalles = async (req, res) => {
-  try {
-    const [result] = await conmysql.query("select * from pedidos_detalle");
-    res.json({ cant: result.length, data: result });
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const getPedidosDetalles = async (req,res)=>{
+    try {
+        const [result] = await conmysql.query('select * from pedidos_detalle')
+        res.json({cant: result.length, data:result})
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const getPedidosdetalleID = async (req, res) => {
-  try {
-    const [result] = await conmysql.query(
-      "select * from pedidos_detalle where det_id=?",
-      [req.params.id]
-    );
-    if (result.length <= 0)
-      return res.status(400).json({
-        det_id: 0,
-        message: "Pedidodetalle no encontrado",
-      });
-    res.json(result[0]);
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const getPedidosdetalleID = async (req,res)=>{
+    try {
+        const [result] = await conmysql.query('select * from pedidos_detalle where det_id=?', [req.params.id])
+        if(result.length<=0) return res.status(400).json({
+            det_id : 0,
+            message : "Pedidodetalle no encontrado"
+        })
+        res.json(result[0])
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const postPedidosDetalle = async (req, res) => {
-  try {
-    const { ped_id, pro_id, det_cantidad } = req.body;
-    const [result] = await conmysql.query(
-      "INSERT INTO pedidos_detalle(ped_id, pro_id, det_cantidad) VALUES (?,?,?)",
-      [ped_id, pro_id, det_cantidad]
-    );
-    res.json({
-      det_id: result.insertId,
-      message: "Detalle de pedido creado exitosamente",
-    });
-  } catch (error) {
-    console.error("Error al crear detalle de pedido:", error);
-    return res
-      .status(500)
-      .json({ message: "Error en el servidor al crear el detalle del pedido" });
-  }
-};
+export const postPedidosdetalle = async (req,res)=>{
+    try {
+        const {prod_id, ped_id, det_cantidad, det_precio} = req.body
+        const [result] = await conmysql.query(
+            'INSERT INTO pedidos_detalle(prod_id, ped_id, det_cantidad, det_precio) VALUES (?,?,?,?)', 
+            [prod_id, ped_id, det_cantidad, det_precio])
+        res.send({
+            id: result.insertId
+        })
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const putPedidosdetalle = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { prod_id, ped_id, det_cantidad, det_precio } = req.body;
-    const [result] = await conmysql.query(
-      "UPDATE pedidos_detalle SET prod_id=?,ped_id=?,det_cantidad=?,det_precio=? WHERE det_id=?",
-      [prod_id, ped_id, det_cantidad, det_precio, id]
-    );
-    if (result.length <= 0)
-      return res.status(404).json({
-        message: "Pedidodetalle no encontrado",
-      });
-    const [row] = await conmysql.query(
-      "select * from pedidos_detalle where det_id=?",
-      [id]
-    );
-    res.json(row[0]);
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const putPedidosdetalle = async (req,res)=>{
+    try {
+        const {id} = req.params
+        const {prod_id, ped_id, det_cantidad, det_precio} = req.body
+        const [result] = await conmysql.query(
+            'UPDATE pedidos_detalle SET prod_id=?,ped_id=?,det_cantidad=?,det_precio=? WHERE det_id=?', 
+            [prod_id, ped_id, det_cantidad, det_precio, id])
+        if(result.length<=0) return res.status(404).json({
+            message : "Pedidodetalle no encontrado"
+        })
+         const [row] = await conmysql.query('select * from pedidos_detalle where det_id=?', [id])
+         res.json(row[0])
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const patchPedidosdetalle = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { prod_id, ped_id, det_cantidad, det_precio } = req.body;
-    const [result] = await conmysql.query(
-      "UPDATE pedidos_detalle SET prod_id=IFNULL(?,prod_id),ped_id=IFNULL(?,ped_id),det_cantidad=IFNULL(?,det_cantidad),det_precio=IFNULL(?,det_precio) WHERE det_id=?",
-      [prod_id, ped_id, det_cantidad, det_precio, id]
-    );
-    if (result.length <= 0)
-      return res.status(404).json({
-        message: "Pedidodetalle no encontrado",
-      });
-    const [row] = await conmysql.query(
-      "select * from pedidos_detalle where det_id=?",
-      [id]
-    );
-    res.json(row[0]);
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const patchPedidosdetalle = async (req,res)=>{
+    try {
+        const {id} = req.params
+        const {prod_id, ped_id, det_cantidad, det_precio} = req.body
+        const [result] = await conmysql.query(
+            'UPDATE pedidos_detalle SET prod_id=IFNULL(?,prod_id),ped_id=IFNULL(?,ped_id),det_cantidad=IFNULL(?,det_cantidad),det_precio=IFNULL(?,det_precio) WHERE det_id=?', 
+            [prod_id, ped_id, det_cantidad, det_precio, id])
+        if(result.length<=0) return res.status(404).json({
+            message : "Pedidodetalle no encontrado"
+        })
+         const [row] = await conmysql.query('select * from pedidos_detalle where det_id=?', [id])
+         res.json(row[0])
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
 
-export const deletePedidosdetalleID = async (req, res) => {
-  try {
-    const [result] = await conmysql.query(
-      "delete from pedidos_detalle where det_id=?",
-      [req.params.id]
-    );
-    if (result.length <= 0)
-      return res.status(400).json({
-        message: "Pedidodetalle no encontrado",
-      });
-    res.sendStatus(204);
-  } catch (error) {
-    return res.status(500).json({ message: " error en el servidor" });
-  }
-};
+export const deletePedidosdetalleID = async (req,res)=>{
+    try {
+        const [result] = await conmysql.query('delete from pedidos_detalle where det_id=?', [req.params.id])
+        if(result.length<=0) return res.status(400).json({
+            message : "Pedidodetalle no encontrado"
+        })
+        res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({message:" error en el servidor"})
+    }
+}
